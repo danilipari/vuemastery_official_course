fe3c9b531e4d0c0b2471ec043b1c0ad9cf77f928
Dani Lipari <lipari.assistenza@gmail.com>
2022-10-23 12:00:59 +0200

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import EventCard from "@/components/EventCard.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          title: "Event 1",
+          date: "2020-01-01",
+          time: "12:00",
+        },
+        {
+          id: 2,
+          title: "Event 2",
+          date: "2020-01-02",
+          time: "12:00",
+        },
+        {
+          id: 3,
+          title: "Event 3",
+          date: "2020-01-03",
+          time: "12:00",
+        },
+      ],
+    };
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  /*  */
+}
 </script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
